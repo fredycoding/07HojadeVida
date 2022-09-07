8fee53bf69d22ad10de6b6c829bf523c2d9ee166
xbox360colombia <xbox360colombia@gmail.com>
2022-09-07 14:57:48 -0500

    Agregando fondo

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -22,16 +22,15 @@ export const Section1 = () => {
     }, [shuffle])
 
   return (
-    <>
+    <div className='imagenpython'>
       <div className="w-full h-screen bg-no-repeat bgimage bg-cover bg-left bg-fixed">
-
         <div className='flex flex-col justify-center h-screen pl-40 relative animate__animated animate__backInRight'>
           <h1 className=' text-6xl font-bold text-slate-600'>Fredy A. Diaz B.</h1>
           <h2 className='mt-4 text-slate-600 text-3xl font-sans tracking-widest'>I'm a <span className=' text-sky-800 animate-pulse'>{newName}</span></h2>
           <p className='mt-4 text-slate-600 text-2xl font-sans tracking-widest'>Developer skills:<span className=' text-sky-800'>Python, Django, .NET, Java, Javascript, C++, React</span></p>
         </div>
       </div>
-    </>
+    </div>
 
 
 
