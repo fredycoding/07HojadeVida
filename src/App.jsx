6afb5288c6b6bd328390ca65314acc9a88bc833b
ASUSTANUS <xbox360colombia@gmail.com>
2022-09-06 20:40:01 -0500

    probando fondo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+
+import { Parallax } from 'react-scroll-parallax'
 import { BotonesMenu } from './components/BotonesMenu'
 import { Section1 } from './components/Section1'
-import { Parallax, ParallaxBannerLayer } from 'react-scroll-parallax'
+
+
+
 function App() {
 
 
--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,8 +1,25 @@
 import './Section1.css'
 import 'animate.css';
+import { useCallback, useEffect, useState } from 'react';
+
+const names = [
+  'Designer', 'Video Editor', 'Audiovisual Producer'
+]
 
 
 export const Section1 = () => {
+
+  const [newName, setnewName] = useState("Developer");
+
+    const shuffle = useCallback(() => {
+        const index = Math.floor(Math.random() * names.length);
+        setnewName(names[index]);
+    }, []);
+
+    useEffect(() => {
+        const intervalID = setInterval(shuffle, 2000);
+        return () => clearInterval(intervalID);
+    }, [shuffle])
 
   return (
     <>
@@ -10,8 +27,7 @@ export const Section1 = () => {
 
         <div className='flex flex-col justify-center h-screen pl-40 relative animate__animated animate__backInRight'>
           <h1 className=' text-6xl font-bold text-slate-600'>Fredy A. Diaz B.</h1>
-          <h2 className='mt-4 text-slate-600 text-3xl font-sans tracking-widest'>I'm a <span className=' text-sky-800'>Developer</span></h2>
-
+          <h2 className='mt-4 text-slate-600 text-3xl font-sans tracking-widest'>I'm a <span className=' text-sky-800 animate-pulse'>{newName}</span></h2>
           <p className='mt-4 text-slate-600 text-2xl font-sans tracking-widest'>Developer skills:<span className=' text-sky-800'>Python, Django, .NET, Java, Javascript, C++, React</span></p>
         </div>
       </div>
