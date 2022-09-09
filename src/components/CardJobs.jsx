f625a5ed2db2beeea79e0ad027fbd8e079e91872
ASUSTANUS <xbox360colombia@gmail.com>
2022-09-09 17:00:00 -0500

    nuevos cambios

--- src/components/CardJobs.jsx
+++ src/components/CardJobs.jsx
@@ -1,17 +1,20 @@
 import { Card } from "flowbite-react"
 
 
-export const CardJobs = () => {
+export const CardJobs = ({titulo}) => {
   return (
-    <div className="max-w-sm">
-  <Card imgSrc="https://flowbite.com/docs/images/blog/image-1.jpg">
-    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-      Noteworthy technology acquisitions 2021
-    </h5>
-    <p className="font-normal text-gray-700 dark:text-gray-400">
-      Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-    </p>
-  </Card>
-</div>
+    <div className="max-w-sm bg-slate-900 bg-opacity-90 rounded-lg border-[0.05rem] border-gray-200 shadow-md">
+
+      <div className="p-5">
+        <a href="#">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-300">{titulo}</h5>
+        </a>
+        <p className="mb-3 font-normal text-slate-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        <button href="#" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white  rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          Read more
+          <svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        </button>
+      </div>
+    </div>
   )
 }
