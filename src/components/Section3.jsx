ca5a45e68696611346f9cccc31fedc9b05ad38f6
ASUSTANUS <xbox360colombia@gmail.com>
2022-09-09 21:25:09 -0500

    nc

--- src/components/Section3.jsx
+++ src/components/Section3.jsx
@@ -11,7 +11,7 @@ export const Section3 = () => {
             <CardJobs titulo="App Rick and Morty" texto="Application made with Javascript, calling an API to read data." imagen="https://user-images.githubusercontent.com/16197568/189462974-70607a39-98c5-4c90-825c-10767628d639.png" url="https://riackandmorty-27d93.web.app/" urlgithub="https://github.com/xbox360colombia/RickandMorty-api-AxiosJS"/>
             <CardJobs titulo="App Trello" texto="Application made with Javascript, Firebase, calling a Firestore Database for save data of To Do list." urlgithub="https://github.com/xbox360colombia/Listado_de_tareas_Firebase_Javascript" url="https://trelloexercise.web.app/" imagen="https://user-images.githubusercontent.com/16197568/180519807-fd246b4b-6898-444f-b377-6818dc433380.png" />
             <CardJobs titulo="App Super Heroes" url="https://coruscating-trifle-23bf34.netlify.app/" urlgithub="https://github.com/xbox360colombia/06MarvelandDc" texto="Marvel and DC application developed in React, which displays the superheroes of these comics" imagen="https://user-images.githubusercontent.com/16197568/189464464-92d5c403-7a4e-4db0-b430-378b6d7c3004.png" />
-            <CardJobs titulo="App Trello" />
+            <CardJobs titulo="To Do React" urlgithub="https://github.com/xbox360colombia/03_ToDo_React" url="https://famous-gumption-e8d962.netlify.app/" imagen="https://user-images.githubusercontent.com/16197568/187051385-180c202d-e78a-49e6-aaab-4b4b44c3108a.png" texto="Application made in React where it has the functions of save, edit and delete" />
             <CardJobs titulo="App Trello" />
             <CardJobs titulo="App Trello" />
           </div>          
