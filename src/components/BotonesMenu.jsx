8af08fc44506e4649c9de36a4dcad6a68b9a081e
ASUSTANUS <xbox360colombia@gmail.com>
2022-09-11 20:58:02 -0500

    Ingresando botones del menú

--- src/components/BotonesMenu.jsx
+++ src/components/BotonesMenu.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
-import iconhome from "../assets/home-icon.svg"
+import OtherHousesIcon from '@mui/icons-material/OtherHouses';
+import Face6Icon from '@mui/icons-material/Face6';
+import AnalyticsIcon from '@mui/icons-material/Analytics';
+import AssuredWorkloadIcon from '@mui/icons-material/AssuredWorkload';
+import ContactsIcon from '@mui/icons-material/Contacts';
 export const BotonesMenu = () => {
   return (
-    <nav className="fixed">
-      <button
-        className='rounded-full bg-blue-700 hover:bg-blue-400 text-white p-2'><img src={iconhome} className="text-white w-8 h-8" /></button>
-      <button
-        className='rounded-full bg-blue-700 hover:bg-blue-400 text-white p-2 mt-4'><img src={iconhome} className="text-white w-8 h-8" /></button>
-    </nav>
+    <>
+    <ul className='fixed z-30 align-middle top-1/3 -mt-24'>
+      <li>
+      <button href="#" className="btn-flotante rounded-full bg-blue-900 ml-10"><OtherHousesIcon/></button>
+      </li>
+      <li>
+      <button href="#" className="btn-flotante rounded-full bg-blue-900 mt-10 ml-10"><Face6Icon/></button>
+      </li>
+      <li>
+      <button href="#" className="btn-flotante rounded-full bg-blue-900 mt-10 ml-10"><AnalyticsIcon/></button>
+      </li> 
+      <li>
+      <button href="#" className="btn-flotante rounded-full bg-blue-900 mt-10 ml-10"><AssuredWorkloadIcon/></button>
+      </li> 
+      <li>
+      <button href="#" className="btn-flotante rounded-full bg-blue-900 mt-10 ml-10"><ContactsIcon/></button>
+      </li>    
+    
+    </ul>
+    </>
   )
 }
